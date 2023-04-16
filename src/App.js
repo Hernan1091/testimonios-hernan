c7cbb0a69c526a6144bfbb108072c1c4ff4a0086
Hernán Cardoso <123619203+Hernan1091@users.noreply.github.com>
2023-04-16 14:37:34 -0600

    Crear archivo para bd e importarlo en app.js

--- src/App.js
+++ src/App.js
@@ -1,41 +1,41 @@
 import './App.css';
 import Testimonio from './componentes/Testimonio.jsx'
 
+import personas from './base-de-datos/desarrolladores-testimonio';
 function App() {
   return (
     <div className="App">
       <div className='contenedor-principal'>
         <h1>Esto es lo que dicen nuestros alumnos de nosotros.</h1>
         <Testimonio 
-        nombre ="Hernán Cardoso"
-        pais= "México"
-        imagen="1"
-        cargo="Ingeniero"
-        empresa="Yosivoy.org"
-        testimonio1="Como desarrollador de software,  y su plataforma de invitaciones digitales, "
-        testimonior = "he tenido la oportunidad de trabajar con yosivoy.org"
-        testimonio2 = "¡y estoy impresionado! La calidad del software y la interfaz de usuario son excepcionales. He tenido una experiencia fluida y sin complicaciones al crear y personalizar invitaciones digitales para eventos. La flexibilidad de la plataforma me permite adaptar las invitaciones a las necesidades de mis clientes y agregar funcionalidades personalizadas. Además, el equipo de soporte técnico de yosivoy.org ha sido extremadamente receptivo y colaborativo, brindándome asistencia rápida y profesional en todo momento."
-        />
+        nombre = {personas[0].nombre}
+        pais= {personas[0].pais}
+        imagen={personas[0].imagen}
+        cargo={personas[0].cargo}
+        empresa={personas[0].empresa}
+        testimonio1={personas[0].testimonio1}
+        testimonior = {personas[0].testimonior}
+        testimonio2 = {personas[0].testimonio2} />
+
         <Testimonio 
-        nombre ="Sebastian Cardoso"
-        pais= "Uruguay"
-        imagen="2"
-        cargo="Ingeniero"
-        empresa="Redy app"
-        testimonio1="Como desarrollador de software,  y su plataforma de invitaciones digitales, "
-        testimonior = "he tenido la oportunidad de trabajar con yosivoy.org"
-        testimonio2 = "¡y estoy impresionado! La calidad del software y la interfaz de usuario son excepcionales. He tenido una experiencia fluida y sin complicaciones al crear y personalizar invitaciones digitales para eventos. La flexibilidad de la plataforma me permite adaptar las invitaciones a las necesidades de mis clientes y agregar funcionalidades personalizadas. Además, el equipo de soporte técnico de yosivoy.org ha sido extremadamente receptivo y colaborativo, brindándome asistencia rápida y profesional en todo momento."
-        />
+        nombre = {personas[1].nombre}
+        pais= {personas[1].pais}
+        imagen={personas[1].imagen}
+        cargo={personas[1].cargo}
+        empresa={personas[1].empresa}
+        testimonio1={personas[1].testimonio1}
+        testimonior = {personas[1].testimonior}
+        testimonio2 = {personas[1].testimonio2} />
+
         <Testimonio 
-        nombre ="Tito Cardoso"
-        pais= "Francia"
-        imagen="3"
-        cargo="Ingeniero"
-        empresa="Visible.com"
-        testimonio1="Como desarrollador de software,  y su plataforma de invitaciones digitales, "
-        testimonior = "he tenido la oportunidad de trabajar con yosivoy.org"
-        testimonio2 = "¡y estoy impresionado! La calidad del software y la interfaz de usuario son excepcionales. He tenido una experiencia fluida y sin complicaciones al crear y personalizar invitaciones digitales para eventos. La flexibilidad de la plataforma me permite adaptar las invitaciones a las necesidades de mis clientes y agregar funcionalidades personalizadas. Además, el equipo de soporte técnico de yosivoy.org ha sido extremadamente receptivo y colaborativo, brindándome asistencia rápida y profesional en todo momento."
-        />
+        nombre = {personas[2].nombre}
+        pais= {personas[2].pais}
+        imagen={personas[2].imagen}
+        cargo={personas[2].cargo}
+        empresa={personas[2].empresa}
+        testimonio1={personas[2].testimonio1}
+        testimonior = {personas[2].testimonior}
+        testimonio2 = {personas[2].testimonio2} />
       </div>
     </div>
   );
---
+++ src/base-de-datos/desarrolladores-testimonio.js
@@ -0,0 +1,35 @@
+
+const personas = [
+    {
+        nombre: "Cuno Cardoso",
+        imagen: 1,
+        pais: "México",
+        cargo: "Desarrollador de software",
+        empresa: "yosivoy.com",
+        testimonio1: "Como desarrollador de software,  y su plataforma de invitaciones digitales,",
+        testimonior: "he tenido la oportunidad de trabajar con yosivoy.org",
+        testimonio2: "¡y estoy impresionado! La calidad del software y la interfaz de usuario son excepcionales. He tenido una experiencia fluida y sin complicaciones al crear y personalizar invitaciones digitales para eventos. La flexibilidad de la plataforma me permite adaptar las invitaciones a las necesidades de mis clientes y agregar funcionalidades personalizadas. Además, el equipo de soporte técnico de yosivoy.org ha sido extremadamente receptivo y colaborativo, brindándome asistencia rápida y profesional en todo momento.",
+    },
+    {
+        nombre: "Julian Cardoso",
+        imagen: 2,
+        pais: "Puebla",
+        cargo: "Desarrollador de software",
+        empresa: "redyapp.mx",
+        testimonio1: "Como desarrollador de software,  y su plataforma de invitaciones digitales,",
+        testimonior: "he tenido la oportunidad de trabajar con yosivoy.org",
+        testimonio2: "¡y estoy impresionado! La calidad del software y la interfaz de usuario son excepcionales. He tenido una experiencia fluida y sin complicaciones al crear y personalizar invitaciones digitales para eventos. La flexibilidad de la plataforma me permite adaptar las invitaciones a las necesidades de mis clientes y agregar funcionalidades personalizadas. Además, el equipo de soporte técnico de yosivoy.org ha sido extremadamente receptivo y colaborativo, brindándome asistencia rápida y profesional en todo momento.",
+    },
+    {
+        nombre: "Marino Cardoso",
+        imagen: 3,
+        pais: "Tamaulipas",
+        cargo: "Desarrollador de software",
+        empresa: "visible.com",
+        testimonio1: "Como desarrollador de software,  y su plataforma de invitaciones digitales,",
+        testimonior: "he tenido la oportunidad de trabajar con yosivoy.org",
+        testimonio2: "¡y estoy impresionado! La calidad del software y la interfaz de usuario son excepcionales. He tenido una experiencia fluida y sin complicaciones al crear y personalizar invitaciones digitales para eventos. La flexibilidad de la plataforma me permite adaptar las invitaciones a las necesidades de mis clientes y agregar funcionalidades personalizadas. Además, el equipo de soporte técnico de yosivoy.org ha sido extremadamente receptivo y colaborativo, brindándome asistencia rápida y profesional en todo momento.",
+    }
+]
+
+export default personas;
